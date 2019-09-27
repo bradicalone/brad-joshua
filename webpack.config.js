7c7aa883e3b11c7efe8fc4cdd825df9211b0ecad
brad vanderbush <Bradical@brads-Mac-mini.local>
2019-09-27 09:21:43 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+//Basically this file is a Node script
+
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    
+    return {
+        entry: './src/main.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node-modules/
+            }]
+        },
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public')
+        },
+        mode: env
+    }
+}
