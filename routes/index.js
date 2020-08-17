7bf02453a52b689f22cc08b84f7e8a96de873104
brad vanderbush <brad@brads-Mac-mini.lan>
2020-08-17 10:52:35 -0700

    Path resolved for sitemap.xml file

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path')
 // let User = require('../models/user.model.js');
-
+console.log(path.resolve('/'))
 
 /* GET contact page. */
 router.get('/contact', (req, res, next) => {
   res.render('contact', { title: 'Contact' });
+});
+
+/* GET sitemap page. */
+router.get('/sitemap.xml', (req, res) => {
+  res.sendFile(path.resolve('sitemap.xml'));
 });
 
 
